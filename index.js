aadb696b1795a12c6ac6d2b167eefda196a87ced
Jonathan Taylor <jonathan.taylor@dsl.pipex.com>
2018-03-22 19:49:31 +0000

    Command line tweaks

--- index.js
+++ index.js
@@ -4,42 +4,41 @@ import axios from "axios";
 // import * as program from "commander";
 const program = require("commander");
 
-import packageJson from "./package.json";
-
 program
-  .version(packageJson.version, "-v, --version")
-  .option("-t, --token [token]", "GitHub API token")
-  .option("-u, --username [username]", "User whose repos should be displayed [taylorjg]", "taylorjg")
+  .option("-t, --token <token>", "GitHub API token")
+  .option("-u, --username <username>", "User whose repos should be displayed", "taylorjg")
   .parse(process.argv);
 
 const GITHUBAPI_BASE_URL = "https://api.github.com";
 const USERNAME = program.username;
 
-const reposUrl = `${GITHUBAPI_BASE_URL}/users/${USERNAME}/repos?per_page=100`;
+const reposUrl = `${GITHUBAPI_BASE_URL}/users/${USERNAME}/repos`;
 
 // TODO: make this the default config for axios so we don't
 //       have to specify it explicitly with each call ?
-const config = program.token ? {
-  headers: {
-    "Authorization": `token ${program.token}`
+const DEFAULT_CONFIG = program.token
+  ? {
+    headers: {
+      "Authorization": `token ${program.token}`
+    }
   }
-} : undefined;
+  : {};
 
 const parseLinkHeader = response => {
   const linkHeader = response.headers["link"];
   if (!linkHeader) {
-      return [];
+    return [];
   }
   return linkHeader
-      .split(",")
-      .map(link => link.split(";").map(bit => bit.trim()))
-      .map(bits => {
-          const bit0 = bits[0];
-          const bit1 = bits[1];
-          const href = /^<(.*)>$/.exec(bit0)[1];
-          const rel = /^rel="(.*)"$/.exec(bit1)[1];
-          return { href, rel };
-      });
+    .split(",")
+    .map(link => link.split(";").map(bit => bit.trim()))
+    .map(bits => {
+      const bit0 = bits[0];
+      const bit1 = bits[1];
+      const href = /^<(.*)>$/.exec(bit0)[1];
+      const rel = /^rel="(.*)"$/.exec(bit1)[1];
+      return { href, rel };
+    });
 };
 
 // TODO: create a generic mechanism to fetch all pages of information
@@ -47,11 +46,14 @@ const parseLinkHeader = response => {
 // - but not specific to any particular operation
 
 const wrapper = async () => {
+  const config = Object.assign({ params: { "per_page": 5 } }, DEFAULT_CONFIG);
   const response = await axios.get(reposUrl, config);
   console.log(response.data.map(x => x.html_url));
   const rels = parseLinkHeader(response);
   console.log(`rels:\n${JSON.stringify(rels, null, 2)}`);
   console.log(`x-ratelimit-remaining: ${response.headers['x-ratelimit-remaining']}`);
+
+  // TODO: add error handling - try/catch
 };
 
 wrapper();
