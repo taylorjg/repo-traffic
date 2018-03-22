ef3b1e022c4ffb5dd1aa3176d25c62ca3afac587
Jonathan Taylor <jonathan.taylor@dsl.pipex.com>
2018-03-22 21:31:59 +0000

    Add axios response interceptor to display remaining rate limit

--- index.js
+++ index.js
@@ -10,14 +10,17 @@ program
   .option("-p, --page-size <n>", "Page size", Number, 100)
   .parse(process.argv);
 
-// TODO: add some sort of interceptor to show URL + remaining rate limit per request
-const myaxios = axios.create({
-  baseURL: "https://api.github.com",
-  headers: program.token
-    ? {
-      "Authorization": `token ${program.token}`
-    }
-    : {}
+axios.defaults.baseURL = "https://api.github.com";
+
+if (program.token) {
+  axios.defaults.headers.common["Authorization"] = `token ${program.token}`;
+}
+
+axios.interceptors.response.use(response => {
+  const request = response.request;
+  const message = `x-ratelimit-remaining: ${response.headers['x-ratelimit-remaining']}`;
+  console.log(`[${request.method} ${request.path}] ${message}`);
+  return response;
 });
 
 const parseLinkHeader = response => {
@@ -38,7 +41,7 @@ const parseLinkHeader = response => {
 };
 
 const getPages = async (url, config) => {
-  const response = await myaxios.get(url, config);
+  const response = await axios.get(url, config);
   const rels = parseLinkHeader(response);
   const next = rels.find(rel => rel.rel === "next");
   if (next) {
@@ -47,6 +50,26 @@ const getPages = async (url, config) => {
   else {
     return [response.data];
   }
+};
+
+const handleError = err => {
+  const SEPARATOR_LINE = "-".repeat(30);
+  console.log(`${SEPARATOR_LINE} START ERROR ${SEPARATOR_LINE}`);
+  if (err.response) {
+    const response = err.response;
+    const request = response.request;
+    const status = response.status;
+    const statusText = response.statusText;
+    const message = `status: ${status}; statusText: ${statusText}`;
+    console.log(`[${request.method} ${request.path}] ${message}`);
+  }
+  else {
+    if (err.config) {
+      console.log(`method: ${err.config.method}; path: ${err.config.url}`);
+    }
+    console.log(`${err}`);
+  }
+  console.log(`${SEPARATOR_LINE}  END ERROR  ${SEPARATOR_LINE}`);
 };
 
 const wrapper = async () => {
@@ -62,23 +85,7 @@ const wrapper = async () => {
     data.forEach((repo, index) => console.log(`[${index}] html_url: ${repo.html_url}`));
   }
   catch (err) {
-    const SEPARATOR_LINE = "-".repeat(30);
-    console.log(`${SEPARATOR_LINE} START ERROR ${SEPARATOR_LINE}`);
-    if (err.response) {
-      const response = err.response;
-      const request = response.request;
-      const status = response.status;
-      const statusText = response.statusText;
-      const message = `status: ${status}; statusText: ${statusText}`;
-      console.log(`[${request.method} ${request.path}] ${message}`);
-      }
-    else {
-      if (err.config) {
-        console.log(`method: ${err.config.method}; path: ${err.config.url}`);
-      }
-      console.log(`${err}`);
-    }
-    console.log(`${SEPARATOR_LINE}  END ERROR  ${SEPARATOR_LINE}`);
+    handleError(err);
   }
 };
 
